978295f48254edf849864b36462f710cdbab4190
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-10-14 14:02:11 +0200

    Añadir lógica de movimiento a la plataforma móvil y optimizar el script del jugador

--- Assets/Script/Jugador.cs
+++ Assets/Script/Jugador.cs
@@ -2,29 +2,20 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed = 5f;
 
-    Rigidbody rb;
-    [SerializeField] private float movementSpeed = 0.1f;
-    float moveDirectionVertical;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        moveDirectionVertical = Input.GetAxis("Vertical");
-    }
+        float moveX = Input.GetAxis("Horizontal"); // A/D o flechas
+        float moveZ = Input.GetAxis("Vertical");   // W/S o flechas
 
-    private void FixedUpdate()
-    {
-        if (moveDirectionVertical > 0)
-        {
-            rb.AddForce(Vector3.forward * movementSpeed, ForceMode.VelocityChange);
-        }
+        Vector3 movimiento = new Vector3(moveX, 0, moveZ) * speed;
+        rb.AddForce(movimiento, ForceMode.VelocityChange);
     }
 }

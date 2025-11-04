6c500856bf11a22ac23227ad77342aabbf911989
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-11-04 14:12:01 +0100

    añado que las balas choquen con los enemigos y estos sean afectados por el impacto

--- Assets/Script/BulletBehaviour.cs
+++ Assets/Script/BulletBehaviour.cs
@@ -2,26 +2,35 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
-
     Rigidbody rb;
-    [SerializeField] float bulletSpeed;
-    [SerializeField] Transform transformPointShoot; 
+    [SerializeField] float bulletSpeed = 30f;
+    [SerializeField] float impactForce = 10f;
+    [SerializeField] float lifeTime = 3f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        transformPointShoot = GameObject.Find("ShootPoint").transform;
+
+        // Lanza la bala hacia adelante según su rotación
+        rb.AddForce(transform.forward * bulletSpeed, ForceMode.VelocityChange);
+
+        // Se destruye sola tras unos segundos para no dejar basura en la escena
+        Destroy(gameObject, lifeTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
+        // Si choca con un enemigo, aplicamos fuerza sin destruir la bala
+        if (other.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRb = other.attachedRigidbody;
 
-    }
-    
-    void FixedUpdate()
-    {
-        rb.AddForce(transformPointShoot.forward * bulletSpeed, ForceMode.VelocityChange);
+            if (enemyRb != null)
+            {
+                // Empuja al enemigo en la dirección de la bala
+                Vector3 direction = transform.forward;
+                enemyRb.AddForce(direction * impactForce, ForceMode.Impulse);
+            }
+        }
     }
 }
--- Assets/Script/Shoot.cs
+++ Assets/Script/Shoot.cs
@@ -8,7 +8,7 @@ public class Shoot : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,7 +22,16 @@ public class Shoot : MonoBehaviour
 
     private void SpawnBullet()
     {
-        Debug.Log("Disparo perra.");
-        Instantiate(bulletPrefab, ShootPoint.position, ShootPoint.rotation);
+        // Instanciamos la bala desde el punto de disparo
+        var bullet = Instantiate(bulletPrefab, ShootPoint.position, ShootPoint.rotation);
+
+        // Ignoramos la colisión entre la bala y el propio jugador
+        Collider playerCollider = GetComponent<Collider>();
+        Collider bulletCollider = bullet.GetComponent<Collider>();
+
+        if (playerCollider != null && bulletCollider != null)
+        {
+            Physics.IgnoreCollision(bulletCollider, playerCollider);
+        }
     }
 }
